52746080b5476c511a75ce50b20b1ddb78225563
John Turner <norgg@norgg.org>
2012-11-16 02:36:14 +0000

    size checks on tokens when loading obj files.

--- objloader.cpp
+++ objloader.cpp
@@ -22,15 +22,15 @@ void objloader::load() {
       vector<string> toks;
       boost::split(toks, line, boost::is_any_of(" /"));
 
-      if (toks[0] == "v") {
+      if (toks[0] == "v" && toks.size() >= 4) {
         vertices.push_back(::atof(toks[1].c_str()));
         vertices.push_back(::atof(toks[2].c_str()));
         vertices.push_back(::atof(toks[3].c_str()));
-      } else if (toks[0] == "vn") {
+      } else if (toks[0] == "vn" && toks.size() >= 4) {
         normals.push_back(::atof(toks[1].c_str()));
         normals.push_back(::atof(toks[2].c_str()));
         normals.push_back(::atof(toks[3].c_str()));
-      } else if (toks[0] == "f") {
+      } else if (toks[0] == "f" && toks.size() >= 7) {
         for (int i = 0; i < 6; i++) {
           faces.push_back(::atoi(toks[i].c_str()));
         }
