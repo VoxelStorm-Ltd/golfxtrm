28510f5f9c900cc34efcad79557d50fa472e5ac3
SlowRiot <slowriot@voxelstorm.com>
2022-10-19 17:52:27 +0100

    bump version to >4.0

--- version.h
+++ version.h
@@ -4,29 +4,29 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "04";
-	static const char MONTH[] = "11";
-	static const char YEAR[] = "2016";
-	static const char UBUNTU_VERSION_STYLE[] =  "16.11";
+	static const char DATE[] = "19";
+	static const char MONTH[] = "10";
+	static const char YEAR[] = "2022";
+	static const char UBUNTU_VERSION_STYLE[] =  "22.10";
 	
 	//Software Status
 	static const char STATUS[] =  "Beta";
 	static const char STATUS_SHORT[] =  "b";
 	
 	//Standard Version Type
-	static const long MAJOR  = 3;
-	static const long MINOR  = 9;
-	static const long BUILD  = 3191;
-	static const long REVISION  = 17377;
+	static const long MAJOR  = 4;
+	static const long MINOR  = 1;
+	static const long BUILD  = 3222;
+	static const long REVISION  = 17549;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 6282;
-	#define RC_FILEVERSION 3,9,3191,17377
-	#define RC_FILEVERSION_STRING "3, 9, 3191, 17377\0"
-	static const char FULLVERSION_STRING [] = "3.9.3191.17377";
+	static const long BUILDS_COUNT  = 6313;
+	#define RC_FILEVERSION 4,1,3222,17549
+	#define RC_FILEVERSION_STRING "4, 1, 3222, 17549\0"
+	static const char FULLVERSION_STRING [] = "4.1.3222.17549";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 91;
+	static const long BUILD_HISTORY  = 22;
 	
 
 }
